1359780e2d9001fd09c3adc9fd750e963e578378
Kenny Peterson <ktpeterson@madisoncollege.edu>
2018-11-12 14:04:38 -0600

    testing push up to group project

---
+++ src/java112/YouSunkMyJavaProject/TeamBattleship.java
@@ -0,0 +1,9 @@
+// I am making the servlet
+
+// Getting the location of the ships from the javabean
+
+// Take fire from url parameter
+// determine if the fire is a hit or a miss
+
+// display the list of hits or misses
+//  display the image of hit/miss/destory
