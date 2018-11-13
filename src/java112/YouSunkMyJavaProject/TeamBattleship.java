f2f3440e3b1fabcf51d70059ac03cea233765708
Kenny Peterson <ktpeterson@madisoncollege.edu>
2018-11-12 21:02:32 -0600

    start to MVC servlet

--- src/java112/YouSunkMyJavaProject/TeamBattleship.java
+++ src/java112/YouSunkMyJavaProject/TeamBattleship.java
@@ -29,16 +29,47 @@ public class TeamBattleship extends HttpServlet {
         String userFired = request.getParameter("userShotFired");
 
         Ship aircraftCarrier = new Ship("aircraftCarrier", ["a1", "a2", "a3", "a4", "a5"], 0);
-        Ship destroyer = new Ship("destoryer", ["b1", "b2", "b3", "b4"], 0);
+        Ship destroyer = new Ship("destroyer", ["b1", "b2", "b3", "b4"], 0);
         Ship battleship = new Ship("battleship", ["c1", "c2", "c3"], 0);
-        Ship cruiser = new Ship("crusier", ["d1"], 0);
+        Ship cruiser = new Ship("cruiser", ["d1"], 0);
         Ship submarine = new Ship("submarine", ["e1", "e2"], 0);
 
-        aircraftCarrier.getLocation();
-        destoryer.getLocation();
-        battleship.getLocation();
-        cruiser.getLocation();
-        submarine.getLocation();
+        ArrayList<String> aircraftCarrierLocation = aircraftCarrier.getLocation();
+        ArrayList<String> destroyerLocation = destroyer.getLocation();
+        ArrayList<String> battleshipLocation = battleship.getLocation();
+        ArrayList<String> cruiserLocation = cruiser.getLocation();
+        ArrayList<String> submarineLocation = submarine.getLocation();
 
+        if (aircraftCarrierLocation.contains(userFired)) {
+            aircraftCarrier.setCurrentHits(1);
+        }
+        if (destroyerLocation.contains(userFired)) {
+            destroyer.setCurrentHits(1);
+        }
+        if (battleshipLocation.contains(userFired)) {
+            battleship.setCurrentHits(1);
+        }
+        if (cruiserLocation.contains(userFired)) {
+            cruiser.setCurrentHits(1);
+        }
+        if (submarineLocation.contains(userFired)) {
+            submarineLocation.setCurrentHits(1);
+        }
+
+        if (aircraftCarrier.isSunk()) {
+            window.alert("You sunk My Java Project... and also " + aircraftCarrier.getName());
+        }
+        if (destroyer.isSunk()) {
+            window.alert("You sunk My Java Project... and also " + destroyer.getName());
+        }
+        if (battleship.isSunk()) {
+            window.alert("You sunk My Java Project... and also " + battleship.getName());
+        }
+        if (cruiser.isSunk()) {
+            window.alert("You sunk My Java Project... and also " + cruiser.getName());
+        }
+        if (submarine.isSunk()) {
+            window.alert("You sunk My Java Project... and also " + submarine.getName());
+        }
      }
 }
