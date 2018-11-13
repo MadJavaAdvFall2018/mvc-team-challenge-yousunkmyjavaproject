1a9678d998d0babf59b1811a6e1570e99d781161
fiveSpeedMazdarati <luke.busch@gmail.com>
2018-11-12 19:54:15 -0600

    created new Ship class

--- src/java112/YouSunkMyJavaProject/Ship.java
+++ src/java112/YouSunkMyJavaProject/Ship.java
@@ -1,4 +1,29 @@
 package java112.YouSunkMyJavaProject;
 
-public class Ship {
+public class Ship extends Object {
+
+    private String name;
+    private int[] shipPosition;
+    private int currentHits;
+    private boolean isSunk;
+
+    /**
+     *
+     * @param name The name of the type of ship
+     * @param shipPosition The upper-left location of the ship
+     * @param currentHits The number of times the ship has been hit
+     */
+    public Ship(String name, int[] shipPosition, int currentHits) {
+
+        this.name = name;
+        this.shipPosition = shipPosition;
+        this.currentHits = currentHits;
+        this.isSunk = false; // a brand new ship is never sunk
+    }
+
+
+
+
+
+
 }
